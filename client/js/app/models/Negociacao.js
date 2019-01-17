b2faedeecff7a558daae0638cf21a34d6d2c4a6e
erick <efmoreira@inf.ufpel.edu.br>
2019-01-17 15:05:35 -0200

    Ooops

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,8 +1,8 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
         this._data = new Date(data.getTime());
-        this._quantidade = 1;
-        this._valor = 0;
+        this._quantidade = quantidade;
+        this._valor = valor;
         Object.freeze(this);
     }
 
